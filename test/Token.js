dd67cf33c4bacde4eba6e40691d05cb8018dbc34
Jimbojay <dekker.i@gmail.com>
2022-08-18 00:06:15 +0200

    7. Token transfer delegation

--- test/Token.js
+++ test/Token.js
@@ -95,7 +95,7 @@ describe('Token', () => {
 			
 			beforeEach(async () => {
 				amount = tokens(100)
-				transaction = await token.connect(deployer).approve(exchange.address, amount)
+				transaction = await token.connect(deployer).approve(exchange.address , amount)
 				result = await transaction.wait()
 			})
 
@@ -121,6 +121,51 @@ describe('Token', () => {
 					await expect(token.connect(deployer).approve('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
 				})
 			})
+
+		})
+
+		describe('Delegated Token Transfers', () => {
+			let amount, transaction, result
+			
+			beforeEach(async () => {
+				amount = tokens(100)
+				transaction = await token.connect(deployer).approve(exchange.address , amount)
+				result = await transaction.wait()
+			})
+
+			describe('Succes', () => {
+				beforeEach(async () => {
+					transaction = await token.connect(exchange).transferFrom(deployer.address, receiver.address , amount)
+					result = await transaction.wait()
+				})
+
+				it('Transfers token balance', async () =>{
+					expect(await token.balanceOf(deployer.address)).to.be.equal(ethers.utils.parseUnits("999900", "ether"))
+					expect(await token.balanceOf(receiver.address)).to.be.equal(amount)
+				})
+
+				it('Resets the allowance', async () => {
+					expect(await token.allowance(deployer.address, exchange.address)).to.be.equal(0)
+				})
+
+				it('Emits an delegated token transfer event', async () => {
+					const event = result.events[0]
+					expect(event.event).to.equal("Transfer")
+
+					const args = event.args
+					expect(args.from).to.equal(deployer.address)
+					expect(args.to).to.equal(receiver.address)
+					expect(args.value).to.equal(amount)
+				})				
+			})
+
+			describe('Failure', async () => {
+				it('Rejects insufficient amounts', async () => {
+					const invalidAmount = tokens(100000000)
+					await expect(token.connect(exchange).transferFrom(deployer.address, receiver.address, invalidAmount)).to.be.reverted
+				})
+			})
+
 
 		})
 
